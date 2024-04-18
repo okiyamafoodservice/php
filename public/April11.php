57fb1c393ec0d945324364cece50068c1055ce61
okiyamafoodservice <naotookiyama@gmail.com>
2024-04-18 16:46:11 +0900

    0418

--- public/April11.php
+++ public/April11.php
@@ -64,5 +64,166 @@ $b = "b";
 echo ($a . $b);
 echo "<hr>";
 
-echo ($a + $b);
+?>
+
+<?php
+
+$a = 8;
+$b = 3;
+
+echo $a + $b . "<br>";
+echo $a - $b . "<br>";
+echo $a * $b . "<br>";
+echo $a / $b . "<br>";
+echo $a % $b . "<br>";
+
+?>
+
+<?php
+
+echo 2+3*100 . "<br>";
+
+echo (2+3)*100 . "<br>";
+
+echo "<hr>";
+?>
+
+<?php
+$true = true;
+$false = false;
+
+$a=$true&&$true;
+$b=$true&&$false;
+$c=$true&&$true&&$true;
+$d=$true&&$true&&$false;
+$e = $true && ($true && $false);
+
+var_dump($a,$b,$c,$d,$e);
+
+echo "<hr>";
+?>
+
+<?php
+$true = true;
+$false = false;
+
+$a=$true||$true;
+$b=$true||$false;
+$c=$true||$true||$true;
+$d=$true||$true||$false;
+$e = $true || ($true || $false);
+$f = $false || $false;
+
+var_dump($a,$b,$c,$d,$e,$f);
+
+echo "<hr>";
+?>
+
+
+
+<?php
+$true = true;
+$false = false;
+
+$a=!$true;
+$b=!$false;
+$c=!$true&&!$true;
+$d=!($true&&$true);
+
+
+var_dump($a,$b,$c,$d);
+echo "<hr>";
+?>
+
+<?php
+
+$a=$true&&$true;
+$b=$true&&$false;
+$c=$true||$true;
+$d=$true||$false;
+$e = !$true;
+$f = !$false;
+$g=!($true&&$true);
+$h=!(true&&$false)&&$true;
+
+var_dump($a,$b,$c,$d,$e,$f,$g,$h);
+echo "<hr>";
+?>
+
+
+<?php
+// 日付を出す関数
+// echo time();
+
+$blog = mktime(15, 40, 0, 4, 18, 2024);
+
+//timeが現在時刻、time()-86400が一日前の時刻
+if($blog>=time()-86400){
+        echo "New!";
+}else{
+        echo "Old!";
+}
+
+echo "<hr>";
+?>
+
+<?php
+
+$age = 19;
+
+if ($age < 20) {
+        echo "購入できません";
+}
+else if($age >= 20 ){
+        echo "年齢を確認する";
+}
+
+else{
+        echo "購入できます";
+}
+
+echo "<hr>";
+
+if ($age < 20) {
+        echo "20歳未満です";
+}
+else if($age >= 20 ){
+        echo "20歳以上です";
+}
+
+else{
+        echo "購入できます";
+}
+
+echo "<hr>";
+?>
+
+<?php
+
+// for:繰り返し処理
+
+
+// for(式1;式2;式3){
+//         処理内容
+// }
+
+//式1:初期化
+//式2:条件式
+//式3:更新処理
+
+for($i=0;$i<10;$i++){
+        echo $i . "<br>";
+}
+
+?>
+
+<?php
+
+for($i=0;$i<=31;$i++){
+        echo $i . "";
+        if($i%7==0){
+                echo "<br>";
+        }
+}
+
 ?>
