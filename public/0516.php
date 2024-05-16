413a8b3aa872bc1abea9da3a1b5ebc3b369fcef3
okiyamafoodservice <naotookiyama@gmail.com>
2024-05-16 14:06:22 +0900

    php

--- public/0516.php
+++ public/0516.php
@@ -3,21 +3,27 @@
 mb_strlen("morning");
 ?>
 
+<br>
+
 引数1とその文字コードを指定したもの
 <?php
 mb_strlen("morning", "UTF-8");
 ?>
 
+<br>
+
 (string $str)
 <?php
 echo strtoupper("morning");
 ?>
+<br>
 
 引数が不要のパターン「void」
 time関数は現在時刻を取得する関数なので、引数が不要です。
 
 echo time();
 
+<br>
 
 <?php
 echo "<hr>";
@@ -25,6 +31,11 @@ echo "<hr>";
 
 Q1
 date("Y-m-d");
+
+時間分秒
+date("H:i:s");
+
+<br>
 
 Q2
 mktime(0, 0, 0, 5, 15, 2024);
