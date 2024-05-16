51880cd975815b5b258ecb5637d142042073e6c2
okiyamafoodservice <naotookiyama@gmail.com>
2024-05-16 14:03:03 +0900

    php

--- public/0516.php
+++ public/0516.php
@@ -1,1 +1,30 @@
+調べたい文字列である引数1のみを指定したもの
 <?php
+mb_strlen("morning");
+?>
+
+引数1とその文字コードを指定したもの
+<?php
+mb_strlen("morning", "UTF-8");
+?>
+
+(string $str)
+<?php
+echo strtoupper("morning");
+?>
+
+引数が不要のパターン「void」
+time関数は現在時刻を取得する関数なので、引数が不要です。
+
+echo time();
+
+
+<?php
+echo "<hr>";
+?>
+
+Q1
+date("Y-m-d");
+
+Q2
+mktime(0, 0, 0, 5, 15, 2024);
